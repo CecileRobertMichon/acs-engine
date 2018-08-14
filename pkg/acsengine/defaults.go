02594177f31611bee809aa00ebeeca7b502fbc77
CecileRobertMichon <cerobert@microsoft.com>
2018-08-14 09:05:48 -0700

    version 0.0.5

--- pkg/acsengine/defaults.go
+++ pkg/acsengine/defaults.go
@@ -97,7 +97,7 @@ var (
 		ImageOffer:     "aks",
 		ImageSku:       "aksbase",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "0.0.4",
+		ImageVersion:   "0.0.5",
 	}
 
 	//DefaultOpenShift39RHELImageConfig is the OpenShift on RHEL distribution.
